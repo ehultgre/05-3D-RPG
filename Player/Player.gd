90a9953928bb849b4cf7c4b41726f7f13c28ccbb
ehultgre <70243373+ehultgre@users.noreply.github.com>
2020-11-29 23:19:27 -0600

    The Rest

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 onready var Camera = get_node("/root/Game/Player/Pivot/Camera")
+onready var Pivot = get_node("/root/Game/Player/Pivot")
 
 var velocity = Vector3()
 var gravity = -9.8
@@ -8,6 +9,8 @@ var speed = 0.2
 var max_speed = 4
 
 var mouse_sensetivity = 0.002
+
+var target = null
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -26,15 +29,30 @@ func _physics_process(_delta):
 	var current_speed = velocity.length()
 	velocity = velocity.normalized() * clamp(current_speed,0,max_speed)
 	velocity.y = falling
-	
-	$AnimationTree.set("parameters/Idle_Run/blend_amount", current_speed/max_speed)
+	if not $AnimationPlayer.is_playing():
+		$AnimationTree.active = true
+		$AnimationTree.set("parameters/Idle_Run/blend_amount", current_speed/max_speed)
 	velocity = move_and_slide(velocity, Vector3.UP, true)
+	
+	if Input.is_action_just_pressed("shoot"):
+		$AnimationTree.active = false
+		$AnimationPlayer.play("Shoot")
+		if target != null and target.is_in_group("target"):
+			target.die()
+	if global_transform.origin.y < -15:
+		get_tree().change_scene("res://UI/Game_Over.tscn")
+	if Global.timer < 0:
+		get_tree().change_scene("res://UI/Game_Over.tscn")
+	if get_node("/root/Game/Target_container").get_child_count() == 0 and get_node("/root/Game/Drone_container").get_child_count() == 0:
+		get_tree().change_scene("res://UI/Win.tscn")
 
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensetivity)
-
+		Pivot.rotate_x(event.relative.y * mouse_sensetivity)
+		Pivot.rotation_degrees.x = clamp(Pivot.rotation_degrees.x, -30, 15)
+ 
 
 func get_input():
 	var input_dir = Vector3()
@@ -48,3 +66,8 @@ func get_input():
 		input_dir += Camera.global_transform.basis.x
 	input_dir = input_dir.normalized()
 	return input_dir
+
+
+func damage():
+	Global.update_score(-5)
+	get_node("/root/Game/UI").add_damage(0.5)
